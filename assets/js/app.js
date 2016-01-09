44b8498ae35c1d255dcf0e92b6d4c3c69ee6749e
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2016-01-09 11:10:56 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,9 @@
 $( document ).ready(function() {
 	/* Sidebar height set */
+        $('button').on('click', function(){
+        	var result = 1 / fake.value;
+        }); 
+   
 	$('.sidebar').css('min-height',$(document).height());
 
 });
